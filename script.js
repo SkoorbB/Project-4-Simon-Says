03afd556724ec36cc2ddbc667b798e68d6708201
Brooks <Skoorb412785@yahoo.com>
2020-02-12 10:43:26 -0500

    game logic turned node list into array and shuffle

--- script.js
+++ script.js
@@ -15,14 +15,49 @@
 //reaches 3
 //alert "you have won"
 
-let square = document.querySelectorAll(".square")
-let start = document.querySelector(".start")
+let emptyArray = [];
 
-start.addEventListener("click", run)
+let square = document.querySelectorAll(".square");
+let start = document.querySelector(".start");        
+
+//turn nodelist into array
+let myArray = Array.from(square)
+
+start.addEventListener("click", run);
+
+//shuffle array
+function shuffle() {
+    let counter = myArray.length;
+
+    // While there are elements in the array
+    while (counter > 0) {
+        // Pick a random index
+        let index = Math.floor(Math.random() * counter);
+
+        // Decrease counter by 1
+        counter--;
+
+        // And swap the last element with it
+        let temp = myArray[counter];
+        myArray[counter] = myArray[index];
+        myArray[index] = temp;
+    }
+
+    return myArray;
+}
 
 function run() {
-    console.log(square)
+    shuffle();
+    console.log(myArray);
+    myArray.push(emptyArray);
+    console.log
+
+//run a shuffle 
+    //for(i = 0; i = square.length; i++) {
+        //square.random
+        //console.l
+
+    }
     //for (i = 0; i > square.length; i++) {
        // square[i].random
     //}
-}
